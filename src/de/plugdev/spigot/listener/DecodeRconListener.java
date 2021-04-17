3bd1e6a03f2113657ffbdc50ea324ca215f6c382
PlugDev <plugdev@outlook.com>
2021-04-17 21:32:25 +0200

    Fixed RCON Threadunsafe to Threadsafe

--- src/de/plugdev/spigot/listener/DecodeRconListener.java
+++ src/de/plugdev/spigot/listener/DecodeRconListener.java
@@ -1,21 +1,32 @@
 package de.plugdev.spigot.listener;
 
+import java.util.concurrent.Callable;
+
 import org.bukkit.Bukkit;
 
+import de.plugdev.spigot.CloudBridge;
 import de.terrarier.netlistening.api.event.DecodeEvent;
 import de.terrarier.netlistening.api.event.DecodeListener;
 
 public class DecodeRconListener implements DecodeListener {
+
+	String nextMessage;
 	
 	@Override
 	public void trigger(DecodeEvent event) {
 		final String key = event.getData().read();
-		if(key.equalsIgnoreCase("rcon")) {
-			String nextMessage;
-			while((nextMessage = event.getData().read()) != null) {
-				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), nextMessage);
+		if (key.equalsIgnoreCase("rcon")) {
+			while ((nextMessage = event.getData().read()) != null) {
+				Bukkit.getScheduler().callSyncMethod(CloudBridge.getCloud(), new Callable<Boolean>() {
+					
+					@Override
+					public Boolean call() {
+						return Bukkit.dispatchCommand(Bukkit.getConsoleSender(), nextMessage);
+					}
+					
+				});
 			}
 		}
 	}
-	
+
 }
